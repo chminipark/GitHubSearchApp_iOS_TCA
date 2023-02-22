eaf52338a42832b7ff8290b956d26a4421509bca
chminipark <chminipark@gmail.com>
2023-02-22 17:56:58 +0900

    First init

---
+++ GitHubSearchApp_iOS_TCA/GitHubSearchApp_iOS_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubSearchApp_iOS_TCAApp.swift
+//  GitHubSearchApp_iOS_TCA
+//
+//  Created by minii on 2023/02/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubSearchApp_iOS_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
